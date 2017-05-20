23b8e7b0c80a77c5ca7336ae24d99f392a8a2e8a
Joseph Sungpil Choi <chousemath@gmail.com>
2017-05-20 11:54:09 +0900

    starting the tutorial over 2

--- text_editor/main.c
+++ text_editor/main.c
@@ -1,6 +1,18 @@
+#include <termios.h>
 #include <unistd.h>
 
+void enableRawMode() {
+    struct termios raw;
+    // read current attributes into a struct
+    tcgetattr(STDIN_FILENO, &raw);
+    // modify the struct by hand
+    raw.c_lflag &= ~(ECHO);
+    // write new terminal attributes out
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+    enableRawMode();
     char c;
     // `read` will read in 1 byte from stdin into `c` until nothing left to read
     // `read` returns the number of bytes that it read, returns 0 when nothing left
